93a44de8f2f8d04bc70d0147a5e77051cba566fc
Ilango <rajagopalilango@gmail.com>
2023-05-30 15:20:19 +0530

    👌 IMPROVE: Add log messages with emojis and colors

--- src/run.rs
+++ src/run.rs
@@ -1,15 +1,37 @@
+use colored::*;
 use std::{env, path::Path, process::Command};
 
 use crate::args::Arguments;
+
+const DONE_EMOJI: &str = "(◠▽◠〃 )";
+const ERROR_EMOJI: &str = "(•⌓• )";
+const WAITING_EMOJI: &str = "（ *ー*）";
 
 pub fn execute_all(args: Arguments) {
     let repo = args.repo.clone();
     let name = args.name.clone();
     let with = args.with;
-    let clone_success = execute_clone(&repo, &name);
+
+    let github_link = if repo.starts_with("https") {
+        repo.to_string()
+    } else {
+        format!("https://github.com/{}", repo)
+    };
+
+    println!(
+        "{} Cloning repo: {}",
+        WAITING_EMOJI.truecolor(255, 99, 202),
+        github_link
+    );
+    let clone_success = execute_clone(&github_link, &name);
 
     let repo_name = String::from(repo.split('/').next().unwrap());
     if clone_success {
+        println!(
+            "{} Successfully cloned: {}",
+            DONE_EMOJI.truecolor(135, 255, 176),
+            github_link
+        );
         let cd_success = match &name {
             Some(name) => execute_cd(name),
             None => execute_cd(&repo_name),
@@ -17,26 +39,30 @@ pub fn execute_all(args: Arguments) {
 
         if cd_success {
             let ca_success = execute_cargo_add(&with);
-            println!("{ca_success}");
+            if ca_success {
+                println!(
+                    "{} Added additional dependencies to Cargo.toml",
+                    DONE_EMOJI.truecolor(135, 255, 176)
+                );
+            } else {
+                println!(
+                    "{} Error adding additional dependencies to Cargo.toml",
+                    ERROR_EMOJI.truecolor(249, 255, 148)
+                );
+            }
         }
     }
 }
 
-fn execute_clone(repo: &String, name: &Option<String>) -> bool {
-    let github_link = if repo.starts_with("https") {
-        repo.to_string()
-    } else {
-        format!("https://github.com/{}", repo)
-    };
-
+fn execute_clone(link: &String, name: &Option<String>) -> bool {
     // TODO: Check if this repo exists first before attempting to clone. If it doesn't exist, throw an error.
     let mut command = Command::new("git");
     match name {
         Some(name) => {
-            command.args(["clone", &github_link, name]);
+            command.args(["clone", link, name]);
         }
         None => {
-            command.args(["clone", &github_link]);
+            command.args(["clone", link]);
         }
     };
 
@@ -69,7 +95,11 @@ fn execute_cargo_add(deps: &Option<String>) -> bool {
             let mut command = Command::new("cargo");
             command.args(args);
 
-            println!("Executing: {:?}", command);
+            println!(
+                "{} Adding additional dependencies: {:?}",
+                WAITING_EMOJI.truecolor(255, 99, 202),
+                mods
+            );
             let status = command.status();
             match status {
                 Ok(status) => status.success(),
@@ -81,3 +111,5 @@ fn execute_cargo_add(deps: &Option<String>) -> bool {
         None => false,
     }
 }
+
+// TODO: Write some tests!!
